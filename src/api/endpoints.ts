4ba1f4b1e54c8885a783c08b29440bdd1592858e
Enkhdavaa Batlkhagva <enkhee3469@gmail.com>
2024-11-20 22:28:32 +0100

    Removed unneccessary files

--- src/api/endpoints.ts
+++
@@ -1,2 +0,0 @@
-export const baseUrl = { hostname: "0.0.0.0", port: 8080 };
-export const baseUrlHostPort = `http://${baseUrl.hostname}:${baseUrl.port}`;
--- src/server/server.ts
+++ src/server/server.ts
@@ -1,9 +1,8 @@
 import { Application } from "@oak/oak";
-import { baseUrl } from "../api/endpoints.ts";
 import { router } from "./router.ts";
 
 const app = new Application();
 app.use(router.allowedMethods());
 app.use(router.routes());
 
-app.listen({ port: baseUrl.port, hostname: baseUrl.hostname });
+app.listen({ port: 8080, hostname: "0.0.0.0" });
